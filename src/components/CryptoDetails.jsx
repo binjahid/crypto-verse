7511aa0f7e224f102ad7f5b291d3fc0f19512a03
Muhammad jahid <jahid78784@gmail.com>
2021-12-18 23:03:32 +0600

    redirects added

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -114,11 +114,11 @@ const CryptoDetails = () => {
             <Option key={date}>{date}</Option>
           ))}
         </Select>
-        <LineChart
+        {/* <LineChart
           coinHistory={coinHistory}
           currentPrice={millify(cryptoDetails.price)}
           coinName={cryptoDetails.name}
-        />
+        /> */}
         <Col className="stats-container">
           <Col className="coin-value-statistics">
             <Col className="coin-value-statistics-heading">
